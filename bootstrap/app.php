cbb34ba5244694fa84e48c2df3dbf04d7b9d8504
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-07-31 18:45:59 +0545

    Refactoring to an singleton.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,24 +1,25 @@
 <?php
 
 use App\Core\App;
+use App\Core\Container;
 use App\Providers\AppServiceProvider;
-use League\Container\Container;
-use App\Core\Example;
 use League\Container\ReflectionContainer;
+
 
 error_reporting(0);
 
+
 require '../vendor/autoload.php';
 
-$container = new Container();
+
+$container = Container::getInstance();
 $container->delegate(new ReflectionContainer() );
 $container->addServiceProvider(new AppServiceProvider());
 
 
-var_dump($container->get(Example::class));
-
-die();
 
 $app = new App();
+$app->run();
+
 
 // register routes
