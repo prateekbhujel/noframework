153b948cc61cfb0b9d4a57bed3bb7b3eb4ca3fd4
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-07-31 12:42:51 +0545

    First commit - building noframework

---
+++ app/Core/App.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Core;
+
+class App 
+{
+
+    public function run()
+    {
+        // Just run our application.
+    }
+
+
+}
---
+++ bootstrap/app.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Core\App;
+
+
+require '../vendor/autoload.php';
+
+
+
+$app = new App();
+
+
+
+var_dump($app);
