1a458b7ac6c9d1e8191c479337838da13d206f26
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-07-31 18:33:43 +0545

    Setting Up autowiring/ to create an instances of the services and dependencies.

---
+++ app/Core/Example.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Core;
+
+
+
+class Example 
+{
+
+ 
+        public function __construct(protected View $view)
+        {
+            
+        }
+
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,24 @@
 <?php
 
+use App\Core\App;
 use App\Providers\AppServiceProvider;
 use League\Container\Container;
+use App\Core\Example;
+use League\Container\ReflectionContainer;
 
 error_reporting(0);
 
 require '../vendor/autoload.php';
 
 $container = new Container();
+$container->delegate(new ReflectionContainer() );
 $container->addServiceProvider(new AppServiceProvider());
 
-var_dump($container->get('name'));
-die();
- 
 
-// Episode : 7 up next (6 Completed)
+var_dump($container->get(Example::class));
+
+die();
+
+$app = new App();
+
+// register routes
