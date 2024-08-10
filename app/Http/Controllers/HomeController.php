ba383e94c772bb5e64966606faf70d7895a993fb
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-08-10 17:41:38 +0545

    Using Service Inside controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Config\Config;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
 
 class HomeController 
 {
 
+    
+    public function __construct(
+        
+        protected Config $config
+
+    ) { }
+
+    
+    
     public function __invoke(ServerRequestInterface $request)
     {
         $response = new Response();
 
-        $response->getBody()->write('<center><h1> Home </h1></center>');
+        $response->getBody()->write($this->config->get('app.name'));
 
         return $response;
         
