3c8b3a33048dd3175a729716161f433cad1ba469
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-08-11 16:25:49 +0545

    Creating an example user profile -- Testing Eloquent.

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,14 @@ use League\Route\Router;
 use Psr\Container\ContainerInterface;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 
 return static function(Router $router, ContainerInterface $container) {
 
     $router->get('/',HomeController::class);
 
     $router->get('/dashboard',DashboardController::class);
+
+    $router->get('/users/{user}',UserController::class);
 
 };
