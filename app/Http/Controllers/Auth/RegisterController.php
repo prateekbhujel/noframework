8fe7c6848e84b8d120e50113b741e93ecce0d7e4
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-08-12 18:35:47 +0545

    Creating the Registration Flow.

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Views\View;
+use Laminas\Diactoros\Response;
+use Psr\Http\Message\ServerRequestInterface;
+
+class RegisterController 
+{
+
+    
+    public function __construct(
+        protected View $view
+
+    ) { }
+
+    
+    
+    public function index(ServerRequestInterface $request)
+    {
+        
+        $response = new Response();
+
+        $response->getBody()->write(
+
+           $this->view->render('auth/register.twig')
+            
+        );
+
+        return $response;
+        
+    }
+
+
+    public function store(ServerRequestInterface $request) 
+    {
+        dump($request->getParsedBody());
+        die();
+
+
+    }
+    
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use League\Route\Router;
 use Psr\Container\ContainerInterface;
 use App\Http\Controllers\HomeController;
@@ -13,5 +14,9 @@ return static function(Router $router, ContainerInterface $container) {
     $router->get('/dashboard',DashboardController::class);
 
     $router->get('/users/{user}',UserController::class);
+
+    $router->get('/register',[RegisterController::class, 'index']);
+
+    $router->post('/register',[RegisterController::class, 'store']);
 
 };
